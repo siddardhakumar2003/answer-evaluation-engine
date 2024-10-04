2fa14fcfeddae12125109745b4dd29e4d5b38835
siddardhakumar2003 <158472582+siddardhakumar2003@users.noreply.github.com>
2024-10-04 06:50:43 +0530

    Update firebase.init.js
    hide api

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC30OvMybgZujaD5itHIeaHidE48RyMFIg",
-  authDomain: "answer-evaluation-engine.firebaseapp.com",
-  projectId: "answer-evaluation-engine",
-  storageBucket: "answer-evaluation-engine.appspot.com",
-  messagingSenderId: "436820827946",
-  appId: "1:436820827946:web:ebe2c234e1f7ade9ab4853",
-  measurementId: "G-Q7MY22JKY1"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
